a53d905f06111d2445beaed720ea922da73a8cbf
Dominik Zulovec Sajovic <dominik.zsajovic@gmail.com>
2022-08-20 00:10:36 +0100

    test of push to main

--- setup.sh
+++
@@ -1,4 +0,0 @@
-
-psql -U postgres -h 127.0.0.1 -f setup\db_setup\db_creation.sql
-
-psql -U postgres -h 127.0.0.1 -d nba -f setup\db_setup\table_creation.sql
--- setup/db_setup/db_creation.sql
+++
@@ -1,5 +0,0 @@
-/*
-This script creates the database for the basketball_scraping project.
-*/
-
-CREATE DATABASE nba;
