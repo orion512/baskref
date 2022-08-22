295f050a7b7e647e78aa07da30c95e2accdf249f
Dominik Zulovec Sajovic <dominik.zsajovic@gmail.com>
2022-08-22 01:42:37 +0100

    added pytest strucuture and started the first test file

---
+++ tests/src/utils/test_date_utils.py
@@ -0,0 +1,38 @@
+"""
+Holds the tests for the date utils functions.
+
+Author: Dominik Zulovec Sajovic - August 2022
+"""
+
+import os
+from datetime import datetime
+from argparse import ArgumentTypeError
+import pytest
+from src.utils.date_utils import valid_date
+
+
+root_path = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir))
+
+
+class TestDateUtils:
+    """Class for date utils tests."""
+
+    test_dates = [
+        ("2022-07-08", datetime.strptime("2022-07-08", "%Y-%m-%d").date()),
+        ("2091-01-03", datetime.strptime("2091-01-03", "%Y-%m-%d").date()),
+        ("1978-12-31", datetime.strptime("1978-12-31", "%Y-%m-%d").date()),
+        ("202207-08", ArgumentTypeError("not a valid date: '202207-08'")),
+        ("2022-13-08", ArgumentTypeError),
+        ("2000/07/08", ArgumentTypeError),
+        ("2022-09-31", ArgumentTypeError),
+        ("20220708", ArgumentTypeError),
+    ]
+
+    @pytest.mark.unittest
+    @pytest.mark.parametrize("str_date, expected_status", test_dates)
+    def test_valid_date(self, str_date, expected_status):
+        """Tests the function valid_date."""
+
+        returned_status = valid_date(str_date)
+
+        assert expected_status == returned_status
