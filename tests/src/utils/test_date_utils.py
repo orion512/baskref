74de1d0f58bdc0c662d65bdbf7656fbfb6520aa8
Dominik Zulovec Sajovic <dominik.zsajovic@gmail.com>
2022-08-23 02:28:32 +0100

    Wrote the tests for valid_date function. The hard part was to figure out how to use the parameterised test when exception is raised

--- tests/src/utils/test_date_utils.py
+++ tests/src/utils/test_date_utils.py
@@ -17,22 +17,32 @@ root_path = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir))
 class TestDateUtils:
     """Class for date utils tests."""
 
-    test_dates = [
+    test_dates_raise = [
+        ("202207-08", pytest.raises(ArgumentTypeError)),
+        ("2022-13-08", pytest.raises(ArgumentTypeError)),
+        ("2000/07/08", pytest.raises(ArgumentTypeError)),
+        ("2022-09-31", pytest.raises(ArgumentTypeError)),
+        ("20220708", pytest.raises(ArgumentTypeError)),
+    ]
+
+    test_dates_correct = [
         ("2022-07-08", datetime.strptime("2022-07-08", "%Y-%m-%d").date()),
         ("2091-01-03", datetime.strptime("2091-01-03", "%Y-%m-%d").date()),
         ("1978-12-31", datetime.strptime("1978-12-31", "%Y-%m-%d").date()),
-        ("202207-08", ArgumentTypeError("not a valid date: '202207-08'")),
-        ("2022-13-08", ArgumentTypeError),
-        ("2000/07/08", ArgumentTypeError),
-        ("2022-09-31", ArgumentTypeError),
-        ("20220708", ArgumentTypeError),
     ]
 
     @pytest.mark.unittest
-    @pytest.mark.parametrize("str_date, expected_status", test_dates)
-    def test_valid_date(self, str_date, expected_status):
+    @pytest.mark.parametrize("str_date, expected_status", test_dates_raise)
+    def test_valid_date_raise(self, str_date, _):
+        """Tests the function valid_date."""
+
+        with pytest.raises(ArgumentTypeError):
+            _ = valid_date(str_date)
+
+    @pytest.mark.unittest
+    @pytest.mark.parametrize("str_date, expected_status", test_dates_correct)
+    def test_valid_date_correct(self, str_date, expected_status):
         """Tests the function valid_date."""
 
         returned_status = valid_date(str_date)
-
         assert expected_status == returned_status
