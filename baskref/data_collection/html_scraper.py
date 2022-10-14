08f1ab4ff76433804c974bb9b1c61ebe90d38871
Dominik Zulovec Sajovic <dominik.zsajovic@gmail.com>
2022-10-14 01:01:06 +0100

    started fixing the html scraper

--- baskref/data_collection/html_scraper.py
+++ baskref/data_collection/html_scraper.py
@@ -7,8 +7,9 @@ Author: Dominik Zulovec Sajovic, September 2022
 
 from typing import Callable, Any, Union
 from dataclasses import dataclass
+import requests
 from requests.exceptions import RequestException
-from requests_html import HTMLSession, HTMLResponse, Element
+from bs4 import BeautifulSoup
 
 
 @dataclass
@@ -57,20 +58,51 @@ class HTMLScraper:
 
     def get_page(self, url: str) -> HTMLResponse:
         """
-        Makes a get request to the provided URL and
-        return a response if status code is ok (200).
+        This function scrapes a static webpage from the web.
+        It implements a strategy to avoid blocking by the host website.
+        1. Normal GET request
+        2. GET request with a proxy IP (if available)
+        3. GET request with a randomized user-agent
+        4. Browser automation (Selenium)
+
+        If the response status code is ok (200-300)
+        the function returns a Response object.
+        Else it raises an error.
         """
 
-        with HTMLSession() as session:
-            try:
-                page = session.get(url)
-            except RequestException as err:
-                raise ScrapingConnError(url) from err
+        # 1. Normal GET request
+        with requests.Session() as session:
+            page = session.get(url)
 
-            if self._is_success_code(page.status_code):
-                return page
+        if self._is_success_code(page.status_code):
+            return page
 
-            raise ScrapingError(url, page.status_code)
+        # TODO: logger here
+        # 2. GET request with a proxy IP (if available)
+        # TODO: implement proxy here
+
+        if self._is_success_code(page.status_code):
+            return page
+
+        # 3. GET request with a randomized user-agent
+        from fake_useragent import UserAgent
+        with requests.Session() as session:
+            # TODO: add proxy here as well
+            page = session.get(
+                url,
+                headers={'User-Agent': UserAgent().random}
+                )
+        
+        if self._is_success_code(page.status_code):
+            return page
+
+        # 4. Browser automation (Selenium, puppeteer)
+        # TODO: implement scrape with browser automation
+        # TODO: form a requests.Response
+        
+
+        raise ScrapingError(url, page.status_code)
+
 
     @staticmethod
     def _is_success_code(code: int) -> bool:
@@ -101,10 +133,10 @@ class ScrapingError(Exception):
         super().__init__(self.message)
 
 
-class ScrapingConnError(Exception):
-    """Definition for a new type of error when connection fails"""
+if __name__ == "__main__":
+    print("Running Script")
 
-    def __init__(self, url: str):
-        """init function"""
-        self.message = f"Connection to {url} failed"
-        super().__init__(self.message)
+    hs = HTMLScraper()
+    pg = hs.get_page("https://www.basketball-reference.com/boxscores/?month=10&day=20&year=2021")
+
+    print(pg)
