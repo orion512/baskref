d1c60ad5342d770189d34c6dfb25781ecde8a494
Dominik Zulovec Sajovic <dominik.zsajovic@gmail.com>
2022-12-10 01:14:12 +0000

    Added a new data point - playing_game. Grouped a few isloated parsings into meta parsing. refactored a try-catch block into a utils function

--- baskref/utils.py
+++ baskref/utils.py
@@ -15,3 +15,18 @@ def valid_date(str_date: str) -> date:
         return datetime.strptime(str_date, "%Y-%m-%d").date()
     except (ValueError, TypeError) as exc:
         raise ArgumentTypeError(f"not a valid date: {str_date!r}") from exc
+
+
+def str_to_datetime(date_str: str, formats: list[str]) -> datetime:
+    """
+    tries to convert a string date into a datetime with multiple formats.
+    If none of the formats work a default datetime is returned.
+    """
+
+    for fmt in formats:
+        try:
+            return datetime.strptime(date_str, fmt)
+        except ValueError:
+            pass
+
+    return datetime(1900, 1, 1)
